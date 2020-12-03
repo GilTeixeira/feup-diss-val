3790259990f913011703e72eb69e49536bf2435a
Gil Teixeira <up201505735@fe.up.pt>
2020-12-03 21:35:57 +0000

    Sonarqube parser now gets from multiple pages

--- sonarqube-parser/sonarqube-parser.js
+++ sonarqube-parser/sonarqube-parser.js
@@ -1,25 +1,37 @@
 const fetch = require("node-fetch");
 const fs = require('fs');
 
-const RESULTS_FILE = 'file_results.txt';
+const PATH_RESULTS =  process.argv[2];
+const SONAR_URL =  process.argv[3];
+const PROJECT_ID = process.argv[4];
+
+const RESULTS_FILE = PATH_RESULTS + 'file_results.txt';
 const LANG = 'java';
-const PROJECT_ID = 'elasticsearch';
 
+console.log()
 
-
-let url = new URL('http://localhost:9000/api/measures/component_tree');
+let url = new URL(SONAR_URL + '/api/measures/component_tree');
 url.search = new URLSearchParams({
-  component:"PROJECT_ID",
+  component:PROJECT_ID,
   metricKeys:"cognitive_complexity,complexity"
 })
 
+console.log(url.toString());
 
-fetch(url)
+fs.writeFileSync(RESULTS_FILE, "file;metric;result\n");
+
+(function fetchData(page=1) 
+{
+fetch(url+"&p="+page)
   .then(response => response.json())
   .then(data => {
     //console.log(data)
 
-    let resultsStr = "file;metric;value\n"
+    let resultsStr = "";
+
+    if(data.components.length === 0)
+      return;
+
     for(let component of data.components){
       let path = component.path;
       //if(!path.endsWith(LANG))
@@ -33,16 +45,21 @@ fetch(url)
           cognicomplex = measure.value;
       }
 
-      resultsStr+=path+";cyclocomplex;"+cyclocomplex+"\n";
-      resultsStr+=path+";cognicomplex;"+cognicomplex+"\n";
+      resultsStr+=path+";OO-CyC;"+cyclocomplex+"\n";
+      resultsStr+=path+";OO-CoC;"+cognicomplex+"\n";
 
     }
 
-    fs.writeFile(RESULTS_FILE, resultsStr, function (err) {
+    fs.appendFile(RESULTS_FILE, resultsStr, function (err) {
         if (err) throw err;
+        //console.log(page++)
+        fetchData(++page);
+        
         console.log('Updated!');
       });
   
   
   });
+})();
+
 
