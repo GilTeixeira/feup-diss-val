dcd3c5b3f33d504a792548ff010ce519418f8bf6
Gil Teixeira <up201505735@fe.up.pt>
2021-01-24 12:29:37 +0000

    Fix Setup

--- run.sh
+++ run.sh
@@ -1,18 +1,16 @@
-#bash ./measure.sh -p $(pwd)/test/elasticsearch/buildSrc -l 'java' -s 'elasticsearch' 
-#sh test.sh -p /home/kristoffer/test.png -l /home/kristoffer/test.txt -s 'phonenumbers-java' 
 
-#bash ./measure.sh -p $(pwd)/test/cpp -l 'cpp' -s 'store-cpp' 
 ## Java Projects
 bash ./measure.sh -p $(pwd)/test/store-java -l 'java' -s 'store-java' 
-#bash ./measure.sh -p $(pwd)/test/libphonenumber/java -l 'java' -s 'libphonenumber-java' -j $(pwd)/test/libphonenumber/java/lib/
-#bash ./measure.sh -p $(pwd)/test/elasticsearch/buildSrc -l 'java' -s 'elasticsearch' -x
+bash ./measure.sh -p $(pwd)/test/libphonenumber/java -l 'java' -s 'libphonenumber-java' -j $(pwd)/test/libphonenumber/java/lib/
+bash ./measure.sh -p $(pwd)/test/elasticsearch/buildSrc -l 'java' -s 'elasticsearch' -x
 
 
 ## JavaScript Projects
-#bash ./measure.sh -p $(pwd)/test/libphonenumber/javascript -l 'javascript' -s 'libphonenumber-js'
-#bash ./measure.sh -p $(pwd)/test/axios/lib -l 'javascript' -s 'axios'
+bash ./measure.sh -p $(pwd)/test/libphonenumber/javascript -l 'javascript' -s 'libphonenumber-js'
+bash ./measure.sh -p $(pwd)/test/axios/lib -l 'javascript' -s 'axios'
 
 
 ## C++ Projects
 #bash ./measure.sh -p $(pwd)/test/store-cpp -l 'cpp' -s 'store-cpp'
 #bash ./measure.sh -p $(pwd)/test/json/single_include/nlohmann -l 'cpp' -s 'nlohmann-json-single'
+#bash ./measure.sh -p $(pwd)/test/simdjson/singleheader -l 'cpp' -s 'simdjson'
--- setup.sh
+++ setup.sh
@@ -6,39 +6,45 @@ npm install --prefix sonarqube-parser/
 npm install --prefix parser/
 
 # ## Setup Analizo
-# git clone https://github.com/GilTeixeira/analizo
-# cd analizo
-# sh development-setup.sh
-# cd ..
+git clone https://github.com/GilTeixeira/analizo
+cd analizo
+sh development-setup.sh
+cd ..
 
 
-# ## Setup ckjm
-# git clone https://github.com/GilTeixeira/ckjm
-# cd ckjm
-# make
-# cd ..
+## Setup ckjm
+git clone https://github.com/GilTeixeira/ckjm
+cd ckjm
+make
+cd ..
 
 
 ## Setup LARA-clava
-# mkdir lara
-# mkdir lara/clava
-# wget http://specs.fe.up.pt/tools/clava.zip -P ./lara/clava
-# unzip lara/clava/clava.zip -d lara/clava/
-# rm lara/clava/clava.zip
+mkdir lara
+mkdir lara/clava
+wget http://specs.fe.up.pt/tools/clava.zip -P ./lara/clava
+unzip lara/clava/clava.zip -d lara/clava/
+rm lara/clava/clava.zip
 
 # ## Setup LARA-kadabra
-# mkdir lara
-# mkdir lara/kadabra
-# wget http://specs.fe.up.pt/tools/kadabra.zip -P ./lara/kadabra
-# unzip lara/kadabra/kadabra.zip -d lara/kadabra/
-# rm lara/kadabra/kadabra.zip
+mkdir lara
+mkdir lara/kadabra
+wget http://specs.fe.up.pt/tools/kadabra.zip -P ./lara/kadabra
+unzip lara/kadabra/kadabra.zip -d lara/kadabra/
+rm lara/kadabra/kadabra.zip
 
 
 ## Setup LARA-Metrics
-# git clone https://github.com/GilTeixeira/feup-diss
+git clone https://github.com/GilTeixeira/feup-diss
 
 
 ## Setup test projects
+cd test
+git clone https://github.com/GilTeixeira/axios
+git clone https://github.com/GilTeixeira/libphonenumber
+git clone https://github.com/GilTeixeira/json
+git clone https://github.com/GilTeixeira/gson
+cd ..
 
 ### Setup elasticsearch
 mkdir -p test
@@ -46,7 +52,3 @@ git clone https://github.com/GilTeixeira/elasticsearch
 mv elasticsearch test
 gradle wrapper -p test/elasticsearch/buildSrc/
 
-#git clone https://github.com/GilTeixeira/axios
-#git clone https://github.com/GilTeixeira/libphonenumber
-#git clone https://github.com/GilTeixeira/json
-#git clone https://github.com/GilTeixeira/gson
